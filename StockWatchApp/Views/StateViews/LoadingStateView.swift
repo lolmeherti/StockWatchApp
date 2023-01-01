7278be9b5b7e004335c36424fcc69d2ffd93c591
Alex H <deampuleadd@yandex.com>
2023-01-01 10:13:17 -0800

    [CHANGE][STATE-VIEW] implements progress circle to loading state view

---
+++ StockWatchApp/Views/StateViews/LoadingStateView.swift
@@ -0,0 +1,26 @@
+//
+//  LoadingStateView.swift
+//  StockWatchApp
+//
+//  Created by Alex H. on 01.01.23.
+//
+
+import SwiftUI
+
+struct LoadingStateView: View {
+    
+    var body: some View {
+        HStack {
+            Spacer()
+            ProgressView()
+                .progressViewStyle(.circular)
+            Spacer()
+        }
+    }
+}
+
+struct LoadingStateView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingStateView()
+    }
+}
